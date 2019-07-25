6a0a6f16f3385dfe8c088570bf57dcea8415b964
mdo <4ryo2-meron-math@ezweb.ne.jp>
2019-07-26 00:06:00 +0900

    add title text mover

--- portfolio/title.js
+++ portfolio/title.js
@@ -4,11 +4,13 @@ const log = function(str){console.log(str)}
 $(function (){
   const selector = $(".title > p");
   addTitleText(selector, selector.text())
-  .then(() => moveTitleText());
+  .then(() => moveTitleText(selector))
+  .then(() => log("end"));
 });
 
 /**
  * @brief タイトルテキストを1文字ずつ表示
+ * @param selector:selector タイトルテキストのセレクタ
  * @param text:string 表示するタイトルテキスト
  * @param time:int 1文字増える時間[ms]
  * @param n:int 表示の最初の値
@@ -32,6 +34,20 @@ const addTitleText = function(selector, text, time=100, n=1){
   });
 }
 
-const moveTitleText = function(){
-  log("ok");
+/**
+ * @brief タイトルテキストを左上に移動
+ * @param text:string 表示するタイトルテキスト
+ * @param time:int 1文字増える時間[ms]
+ * @param n:int 表示の最初の値
+ * @return Promise(void) 返り値なし
+ */
+const moveTitleText = function(selector, time1=1500, time2=1000){
+  setTimeout(function(){
+    return selector.animate({
+      "font-size": "100%",
+      "width": "300px",
+      "margin-left": "0px",
+      "top": "10px"
+    }, time1).promise();
+  }, time2);
 }

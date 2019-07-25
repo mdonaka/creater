b80e363ca6e4ee49222d43ab54ede83ec43b03f7
mdo <4ryo2-meron-math@ezweb.ne.jp>
2019-07-25 22:26:28 +0900

    add title text generator

--- portfolio/title.js
+++ portfolio/title.js
@@ -2,13 +2,36 @@
 const log = function(str){console.log(str)}
 
 $(function (){
-  setTitleText(1);
+  const selector = $(".title > p");
+  addTitleText(selector, selector.text())
+  .then(() => moveTitleText());
 });
 
-const text = "hello!";
-const setTitleText = function(n){
-  $(".title > p").text(text.slice(0,n));
-  if(n < text.length){
-    //setTimeout(setTitleText(n+1), 1000);
-  }
-};
+/**
+ * @brief タイトルテキストを1文字ずつ表示
+ * @param text:string 表示するタイトルテキスト
+ * @param time:int 1文字増える時間[ms]
+ * @param n:int 表示の最初の値
+ * @return Promise(void) 返り値なし
+ */
+const addTitleText = function(selector, text, time=100, n=1){
+  return new Promise(function(resolve){
+    setTimeout(function(){
+      if(n < text.length){
+        // テキスト更新
+        selector.text(text.slice(0,n)+"|");
+        // 次の文字の更新処理
+        addTitleText(selector, text, time, n+1)
+        .then(() => resolve());
+      } else {
+        // テキスト操作の終了
+        selector.text(text);
+        resolve();
+      }
+    }, time);
+  });
+}
+
+const moveTitleText = function(){
+  log("ok");
+}
